c32937d1e64f148946027fbcfd25992f466c49bf
ShAoN <27mdshaonali@gmail.com>
2025-05-16 15:23:24 +0600

    Files added 1. Sign in page design has completed 2. added Sign Up Activity.

--- app/src/main/java/com/binarybirds/locallaundry/SignUpActivity.java
+++ app/src/main/java/com/binarybirds/locallaundry/SignUpActivity.java
@@ -1,45 +1,24 @@
 package com.binarybirds.locallaundry;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    AppCompatTextView signUp;
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        initializeView();
-        signUp.setOnClickListener(v -> setSignUp());
-
     }
-
-    public void initializeView() {
-
-        signUp = findViewById(R.id.signUp);
-
-    }
-
-    public void setSignUp() {
-
-        startActivity(new Intent(this, SignUpActivity.class));
-
-    }
-
 }
