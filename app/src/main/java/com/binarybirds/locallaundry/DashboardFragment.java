bccc3258f0fdb5a1ca12183b40cbc254002debe4
ShAoN <27mdshaonali@gmail.com>
2025-05-18 01:26:52 +0600

    Files added 1. Sign in page design has completed 2. Added Sign Up Activity. 3. Working on UserDashboard 4. Firebase:        1. Firebase Analytic has added        2. Firebase In-App Messaging has added

---
+++ app/src/main/java/com/binarybirds/locallaundry/DashboardFragment.java
@@ -0,0 +1,21 @@
+package com.binarybirds.locallaundry;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class DashboardFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return myView;
+    }
+
+
+}
