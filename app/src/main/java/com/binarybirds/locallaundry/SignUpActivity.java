9199df57108d39cf70638f65e799ca6b6c0c5644
ShAoN <27mdshaonali@gmail.com>
2025-05-19 05:00:46 +0600

    Files added 1. Sign in page design has completed 2. Added Sign Up Activity. 3. Working on UserDashboard 4. Firebase:        1. Firebase Analytic has added
           2. Firebase In-App Messaging has added
    
           3. Firebase Custom Cloud Messaging/Push Notification from my server "https://codecanvas.top/Laundry%20StartUp/Firebase/firebase.php" has been added
    
           4. Firebase Background SMS send remotely from my server "https://codecanvas.top/Laundry%20StartUp/Firebase/firebase.php" has been added

--- app/src/main/java/com/binarybirds/locallaundry/SignUpActivity.java
+++ app/src/main/java/com/binarybirds/locallaundry/SignUpActivity.java
@@ -2,13 +2,21 @@ package com.binarybirds.locallaundry;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatTextView;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -24,22 +32,115 @@ public class SignUpActivity extends AppCompatActivity {
         });
 
         initializeView();
-
     }
 
     public void initializeView() {
-
         AppCompatTextView signIn = findViewById(R.id.signIn);
+        TextInputEditText userSignUpFullName = findViewById(R.id.userSignUpFullName);
+        TextInputEditText userSignUpEmail = findViewById(R.id.userSignUpEmail);
+        TextInputEditText userSignUpEnterPassword = findViewById(R.id.userSignUpEnterPassword);
+        TextInputEditText userSignUpReenterPassword = findViewById(R.id.userSignUpReenterPassword);
+        AppCompatButton signUpButton = findViewById(R.id.signUpButton);
+
+        signUpButton.setEnabled(false);
+
+        TextWatcher inputWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                validateFields(userSignUpFullName, userSignUpEmail, userSignUpEnterPassword, userSignUpReenterPassword, signUpButton);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        userSignUpFullName.addTextChangedListener(inputWatcher);
+        userSignUpEmail.addTextChangedListener(inputWatcher);
+        userSignUpEnterPassword.addTextChangedListener(inputWatcher);
+        userSignUpReenterPassword.addTextChangedListener(inputWatcher);
 
         signIn.setOnClickListener(view -> signIn());
-
     }
 
+    private void validateFields(TextInputEditText fullNameField, TextInputEditText emailField, TextInputEditText passwordField, TextInputEditText rePasswordField, AppCompatButton signUpButton) {
+
+        String fullName = fullNameField.getText().toString().trim();
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString();
+        String rePassword = rePasswordField.getText().toString();
+
+        boolean allFilled = !fullName.isEmpty() && !email.isEmpty() && !password.isEmpty() && !rePassword.isEmpty();
+        boolean passwordsMatch = password.equals(rePassword);
+
+        boolean isLengthValid = password.length() >= 8;
+        boolean hasNumber = password.matches(".*\\d.*");
+        boolean hasLower = password.matches(".*[a-z].*");
+        boolean hasUpper = password.matches(".*[A-Z].*");
+        boolean hasSpecial = password.matches(".*[!@#$%^&*()_+=<>?/{}~`|\\[\\]\\-].*");
+
+        boolean allPasswordConditionsMet = isLengthValid && hasNumber && hasLower && hasUpper && hasSpecial;
+
+        TextView mustContainsNumber = findViewById(R.id.mustContainsNumber);
+        TextView mustContainsChar = findViewById(R.id.mustContainsChar);
+        TextView mustContainsLowerCase = findViewById(R.id.mustContainsLowerCase);
+        TextView mustContainsLowerUpperCase = findViewById(R.id.mustContainsLowerUpperCase);
+        TextView mustContainsSpecialSymbol = findViewById(R.id.mustContainsSpecialSymbol);
+
+        int defaultColor = ContextCompat.getColor(this, R.color.textSecondary);
+        int successColor = ContextCompat.getColor(this, R.color.colorSuccess);
+
+        mustContainsChar.setCompoundDrawablesWithIntrinsicBounds(R.drawable.dot, 0, 0, 0);
+        mustContainsChar.setTextColor(defaultColor);
+
+        mustContainsNumber.setCompoundDrawablesWithIntrinsicBounds(R.drawable.dot, 0, 0, 0);
+        mustContainsNumber.setTextColor(defaultColor);
+
+        mustContainsLowerCase.setCompoundDrawablesWithIntrinsicBounds(R.drawable.dot, 0, 0, 0);
+        mustContainsLowerCase.setTextColor(defaultColor);
+
+        mustContainsLowerUpperCase.setCompoundDrawablesWithIntrinsicBounds(R.drawable.dot, 0, 0, 0);
+        mustContainsLowerUpperCase.setTextColor(defaultColor);
+
+        mustContainsSpecialSymbol.setCompoundDrawablesWithIntrinsicBounds(R.drawable.dot, 0, 0, 0);
+        mustContainsSpecialSymbol.setTextColor(defaultColor);
+
+        if (!passwordsMatch && !rePassword.isEmpty()) {
+            //rePasswordField.setError("Passwords do not match");
+            Log.d("SignUpActivity", "Passwords do not match");
+        } else {
+            rePasswordField.setError(null);
+        }
+
+        if (isLengthValid) {
+            mustContainsChar.setCompoundDrawablesWithIntrinsicBounds(R.drawable.check_mark, 0, 0, 0);
+            mustContainsChar.setTextColor(successColor);
+        }
+        if (hasNumber) {
+            mustContainsNumber.setCompoundDrawablesWithIntrinsicBounds(R.drawable.check_mark, 0, 0, 0);
+            mustContainsNumber.setTextColor(successColor);
+        }
+        if (hasLower) {
+            mustContainsLowerCase.setCompoundDrawablesWithIntrinsicBounds(R.drawable.check_mark, 0, 0, 0);
+            mustContainsLowerCase.setTextColor(successColor);
+        }
+        if (hasUpper) {
+            mustContainsLowerUpperCase.setCompoundDrawablesWithIntrinsicBounds(R.drawable.check_mark, 0, 0, 0);
+            mustContainsLowerUpperCase.setTextColor(successColor);
+        }
+        if (hasSpecial) {
+            mustContainsSpecialSymbol.setCompoundDrawablesWithIntrinsicBounds(R.drawable.check_mark, 0, 0, 0);
+            mustContainsSpecialSymbol.setTextColor(successColor);
+        }
+
+        signUpButton.setEnabled(allFilled && passwordsMatch && allPasswordConditionsMet);
+    }
 
     public void signIn() {
-
         startActivity(new Intent(this, MainActivity.class));
-
     }
-
 }
